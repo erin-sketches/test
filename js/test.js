87480a0432e9256b1b5ade62d31b984f0fef279a
erin <erin.sketches@gmail.com>
2019-12-28 15:28:30 -0500

    Revert "Add pan/zoom and refactor a bit"
    This reverts commit e36d56cc07fcb6cc79b7ed1ec0daefb619ce774b.

--- js/test.js
+++ js/test.js
@@ -1,38 +1,45 @@
 import * as THREE from './ext/three.module.js';
 import * as dat from './ext/dat.gui.module.js';
-import * as zoom from './zoom.js';
+import * as d3 from './ext/d3.v5.min.js';
 
-// utilities
-class ColorGUIHelper {
-	constructor(object, prop) {
-		this.object = object;
-	    this.prop = prop;
-	}
-  	get value() {
-	    return `#${this.object[this.prop].getHexString()}`;
-	}
-  	set value(hexString) {
-	    this.object[this.prop].set(hexString);
-	}
-}
-
-var view = new zoom.View(window.innerWidth,
-					 window.innerHeight);
 var scene = new THREE.Scene();
-var geometry = new THREE.BoxGeometry( 50, 50, 50 );
+var camera = new THREE.PerspectiveCamera( 50, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { color: 0x220022 } );
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
+camera.position.z = 5;
+
+var render = function () {
+	renderer.render( scene, camera );
+};
 var animate = function () {
 	requestAnimationFrame( animate );
 
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
-	view.render(scene);
+	render();
 };
 
 animate();
+class ColorGUIHelper {
+	  constructor(object, prop) {
+		      this.object = object;
+		      this.prop = prop;
+		    }
+	  get value() {
+		      return `#${this.object[this.prop].getHexString()}`;
+		    }
+	  set value(hexString) {
+		      this.object[this.prop].set(hexString);
+		    }
+}
 window.addEventListener('DOMContentLoaded',() => {
 	const datGui  = new dat.GUI({ autoPlace: true });
 	  
@@ -42,12 +49,14 @@ window.addEventListener('DOMContentLoaded',() => {
 	
 	folder.addColor(new ColorGUIHelper(material,'color'),'value')
 	  .name('color')
-	  .onChange(view.render(scene));
-	let fov = folder.add(view.cam, 'fov', 10, 100);
+	  .onChange(render);
+	let fov = folder.add(camera, 'fov', 10, 100);
 	fov.onChange(function(value) {
-		view.cam.updateProjectionMatrix();
+		camera.updateProjectionMatrix();
 	})
 })
 window.addEventListener('resize', () => {
-	view.resize(window.innerWidth,window.innerHeight);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	camera.aspect = window.innerWidth/window.innerHeight;
+	camera.updateProjectionMatrix();
 });
